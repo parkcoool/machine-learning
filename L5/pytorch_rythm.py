a42fc6b1efb4709b09f6db44a32026ac7c59821a
parkcoool <yickle1345@naver.com>
2024-01-07 22:13:33 +0900

    Create pytorch_rhythm in L5

---
+++ L5/pytorch_rythm.py
@@ -0,0 +1,30 @@
+# %%
+import torch
+
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[2.0], [4.0], [6.0]])
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+
+model = Model()
+criterion = torch.nn.MSELoss(size_average=False)
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(500):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
